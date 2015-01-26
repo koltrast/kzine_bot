721f882a6d820c21cfdea1e3e6b5740be41d7800
koltrast <rupus@reinefjord.se>
2015-01-26 18:27:14 +0100

    code cleanup to comply with PEP 8

--- kzine_bot.py
+++ kzine_bot.py
@@ -5,7 +5,7 @@ import requests
 import feedparser
 import json
 import time
-import datetime
+from datetime import datetime
 from operator import attrgetter
 
 class Reddit: 
@@ -17,11 +17,14 @@ class Reddit:
         self.username = username
         self.password = password
 
-        user_pass_dict = {'user': self.username,
-                          'passwd': self.password,
-                          'api_type': 'json'}
+        user_pass_dict = {
+            'user': self.username,
+            'passwd': self.password,
+            'api_type': 'json'
+        }
 
-        r = self.session.post(r'http://www.reddit.com/api/login', data = user_pass_dict)
+        r = self.session.post(r'http://www.reddit.com/api/login',
+            data = user_pass_dict)
         r_json = json.loads(r.content.decode())
         
         try:
@@ -29,28 +32,24 @@ class Reddit:
         except:
             print(r_json)
 
-    def get_me(self):
-        r = self.session.get(r'http://www.reddit.com/api/me.json')
-        r_json = json.loads(r.content.decode())
-       
-        # Recieve a new modhash from me request 
-        self.modhash = r_json['json']['data']['modhash']
-        
-        return r_json
-
     def submit_link(self, link, title, subreddit):
-        submission_dict = {'kind': 'link',
-                           'url': link,
-                           'title': title,
-                           'sr': subreddit,
-                           'uh': self.modhash,
-                           'api_type': 'json'}
+        submission_dict = {
+            'kind': 'link',
+            'url': link,
+            'title': title,
+            'sr': subreddit,
+            'uh': self.modhash,
+            'api_type': 'json'
+        }
 
-        r = self.session.post(r'http://www.reddit.com/api/submit', data = submission_dict)
+        r = self.session.post(r'http://www.reddit.com/api/submit',
+            data = submission_dict)
         r_json = json.loads(r.content.decode())
 
     def get_submissions(self):
-        r = self.session.get(r'http://www.reddit.com/user/{}/submitted.json'.format(self.username))
+        r = self.session.get(
+            r'http://www.reddit.com/user/{}/submitted.json'.format(
+            self.username))
         r_json = json.loads(r.content.decode())
         return r_json
 
@@ -71,10 +70,12 @@ def get_entries(reddit_submitted, url):
     reddit_submitted_urls = []
     unsubmitted = []
     for index in range(len(reddit_submitted['data']['children'])):
-        reddit_submitted_urls.append(reddit_submitted['data']['children'][index]['data']['url'])
+        reddit_submitted_urls.append(
+            reddit_submitted['data']['children'][index]['data']['url'])
 
     for index in range(len(feed['entries'])):
-        if (feed['entries'][index]['links'][0]['href'] in reddit_submitted_urls) == False:
+        if (feed['entries'][index]['links'][0]['href'] 
+                in reddit_submitted_urls) == False:
             unsubmitted.append(feed['entries'][index])
         else: 
             break
@@ -82,11 +83,12 @@ def get_entries(reddit_submitted, url):
     return unsubmitted
 
 def get_current_entries(entries):
-    time_now = datetime.datetime.now() 
+    time_now = datetime.now() 
     to_submit = []
 
     for entry in entries:
-        entry_time = datetime.datetime.fromtimestamp(time.mktime(entry['published_parsed']))
+        entry_time = datetime.fromtimestamp(
+            time.mktime(entry['published_parsed']))
         time_delta = time_now - entry_time
 
         if time_delta.days < 1:
